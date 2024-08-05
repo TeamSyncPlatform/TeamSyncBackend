2d40f1c62df541439b86b6319c75321b5a44f1a2
Ilija Bešlin <44114597+GearFifth@users.noreply.github.com>
2024-08-05 12:15:36 +0200

    feat: initialized spring boot project

---
+++ TeamSync/src/main/java/com/teamsync/TeamSync/TeamSyncApplication.java
@@ -0,0 +1,13 @@
+package com.teamsync.TeamSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamSyncApplication.class, args);
+	}
+
+}
---
+++ TeamSync/src/test/java/com/teamsync/TeamSync/TeamSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsync.TeamSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
